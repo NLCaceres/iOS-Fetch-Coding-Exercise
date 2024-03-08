0152e0c50732a982e4d60944eafd18398dc7c0e5
NLCaceres <NLCaceres21@Gmail.com>
2024-03-07 20:54:57 -0800

    Initial Commit

---
+++ FetchMealCodingExercise/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FetchMealCodingExercise
+//
+//  Created by Nick Caceres on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FetchMealCodingExercise/FetchMealCodingExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMealCodingExerciseApp.swift
+//  FetchMealCodingExercise
+//
+//  Created by Nick Caceres on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchMealCodingExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
