0d99f332d6a8438609b0125a8849f00497415212
NLCaceres <NLCaceres21@Gmail.com>
2024-03-13 19:41:45 -0700

    Add computed prop to adjust Centered ProgressView's color from black to white for dark mode

--- FetchMealCodingExercise/Views/Reusable/CenteredProgressView.swift
+++ FetchMealCodingExercise/Views/Reusable/CenteredProgressView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct CenteredProgressView: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    // Color the ProgressView itself and its text for best visibility in both light and dark mode
+    var viewColor: Color { return colorScheme == .light ? .black : .white }
+
     var body: some View {
         VStack {
             Spacer()
             HStack {
                 Spacer()
                 ProgressView {
-                    Text("Loading").foregroundColor(.black).font(.title3)
-                }.frame(width: 100, height: 100).tint(.black)
+                    Text("Loading")
+                        .font(.title3).foregroundColor(viewColor)
+                }.frame(width: 100, height: 100).tint(viewColor)
                 Spacer()
             }
             Spacer()
