714bb0291b0af803275359b0b9a1211c511ea094
NLCaceres <NLCaceres21@Gmail.com>
2024-03-13 18:50:37 -0700

    Adding "isLoading" property to Dessert List ViewModel to track Meal List fetching

--- FetchMealCodingExercise/Views/DessertList/DessertListViewModel.swift
+++ FetchMealCodingExercise/Views/DessertList/DessertListViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class DessertListViewModel: ObservableObject {
     let mealAPI: MealApiService
 
+    @Published private(set) var isLoading = false
     @Published private(set) var errorMessage = ""
     @Published private(set) var meals: [Meal] = []
     
@@ -20,7 +21,11 @@ class DessertListViewModel: ObservableObject {
     // MainActor ensures publishing occurs on the Main thread while the network request occurs in the background
     @MainActor
     func getDessertMeals() async {
+        isLoading = true
+        defer { isLoading = false } // Runs no matter what (similar to a Java finally block)
+
         errorMessage = ""
+
         do {
             meals = try await mealAPI.getMealListFiltered(byCategory: "Dessert").sorted {
                 // Sorted by name alphabetically a-Z
